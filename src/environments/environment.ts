cd40db8924cdbd35c090e74b5b104bf2f19f23dd
Steven Halase <steven.halase@gmail.com>
2022-03-27 19:08:37 -0600

    Removing dotenv/ts-node and just commiting environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  ETHERSCAN_API_KEY: '',
-  NOMICS_API_KEY: '',
+  ETHERSCAN_API_KEY: 'ANI6819XGM4JD8RDBE5Z3PSQ1M49G81JP4',
+  NOMICS_API_KEY: '1ffec1319bb59391351d47c7bcf780fcab2ffc0f',
   production: 'true',
 };
